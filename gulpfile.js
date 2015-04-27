a931a74afdd91106d59423d046e68cc8cb83df0e
Denjell <denjell@das-gaengeviertel.info>
2015-04-27 23:42:14 +0200

    material design implemented in mixins

--- gulpfile.js
+++ gulpfile.js
@@ -53,20 +53,16 @@ gulp.task('js-deps', ['modernizr'], function () {
         'bower_components/ng-file-upload/angular-file-upload-shim.min.js',
         'lib/modernizr/modernizr-custom.min.js',
         'bower_components/angular/angular.js',
-        'bower_components/angular/angular.js',
-        'bower_components/angular-aria/angular-aria.js',
-        'bower_components/angular-material/angular-material.js',
-        'bower_components/angular-route/angular-route.js',
         'bower_components/angular-animate/angular-animate.js',
+        'bower_components/angular-aria/angular-aria.js',
+        'bower_components/angular-route/angular-route.js',
+        'bower_components/angular-messages/angular-route.js',
+        'bower_components/angular-material/angular-material.js',
         'bower_components/angular-busy/angular-busy.js',
         'bower_components/ng-file-upload/angular-file-upload.js',
-        'bower_components/async/lib/async.js',
         'bower_components/angular-locker/dist/angular-locker.js',
         'bower_components/angular-touch/angular-touch.js',
-        'bower_components/angular-touch/angular-touch.js',
-        'bower_components/angular-touch/angular-touch.js',
-        'bower_components/angular-touch/angular-touch.js',
-
+        'bower_components/async/lib/async.js'
 
     ])
         .pipe(concat('ng-ito-dataviewer-dependencies.min.js'))
--- src/web/js/controllers/site.js
+++ src/web/js/controllers/site.js
@@ -1,23 +1,73 @@
 /* global angular */
 (function () {
     'use strict';
-    angular.module('ito-dataviewer.welcome', [])
+    angular.module('ito-dataviewer.welcome', ['ngMaterial'])
         .controller('Site.Welcome', ['$scope', function ($scope) {
                 $scope.assets = {
                     asset : {
+                        _id : '00001',
                         title: "thing",
-                        description: "more and more stuff",
+                        description: null,
                         category: {
-                            thing2: "wow",
-                            thing4: "wowow"
+                            thing2: "wow"
                         },
+                        license: true,
                         visible: true,
-                        privacy: false,
+                        privacy: true,
                         editable: true
                     }
                 };
+                $scope._sys = {
+                    visibility : {
+                        one:{
+                            title: 'public'
+                        },
+                        two:{
+                            title: 'private'
+                        },
+                        three:{
+                            title: 'trashed'
+                        },
+                        four:{
+                            title: 'banned'
+                        }
+                    },
+                    licenses : {
+                        copyright : {
+                            title : 'copyright',
+                            text : 'Copyright is a thing'
+                        },
+                        copyleft : {
+                            title : 'copyleft',
+                            text : 'Copyleft is a thing'
+                        }
+                    },
+                    categories : {
+                        one : {
+                            title: 'wow'
+                        },
+                        two : {
+                            title: 'wowo'
+                        },
+                        three : {
+                            title: 'wowow'
+                        }
+                    }
+                };
             $scope.$apply();
+            /*
+            $scope.clearValue = function(model) {
+                $scope.model = undefined;
+            };
+            $scope.save = function() {
+                alert('Form was valid!');
+            };
+            $scope.i18n = function (string,language) {
+
+            }
+            */
         }])
+
         .controller('Site.About', ['$scope', function ($scope) {
             $scope.$parent.status = 'ready';
         }])
